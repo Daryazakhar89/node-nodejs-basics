5e262995c283546d1fef78df79adc24dc7675e5e
Darya Zakhar <daryazakhar.89@gmail.com>
2023-04-05 17:59:38 +0400

    feat: implement function that prints all array of filenames from folder

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import {readdir} from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const pathToFolder = path.join(__dirname, '/files');
+
+    try {
+        const arrayOfFileNames = await readdir(pathToFolder);
+        console.log(arrayOfFileNames);
+    } catch (err) {
+        throw new Error ('FS operation failed');
+    }
 };
 
 await list();
