3cad993f32fa2b940e4b96ce478b2778632b4f88
Darya Zakhar <daryazakhar.89@gmail.com>
2023-04-05 18:00:08 +0400

    feat: implement function that prints content of the file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import {readFile} from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, '/files/fileToRead.txt');
+
+    try {
+        const content = await readFile(pathToFile, 'utf-8');
+        console.log(content);
+    } catch (err) {
+        throw new Error ('FS operation failed');
+    }
 };
 
 await read();
