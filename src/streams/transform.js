2cbb75636dfcf9ab380c9d594c1576c6ee4919ac
Darya Zakhar <daryazakhar.89@gmail.com>
2023-04-07 01:00:27 +0400

    feat: implement function that transform data using transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import { pipeline } from 'stream/promises';
+import { Transform } from 'stream';
+
+const writableStream = process.stdout;
+const readableStream = process.stdin;
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform ({
+        transform(chunk, encoding, callback) {
+
+            const chunkString = chunk.toString().trim();
+            const reverseChankString = chunkString.split('').reverse().join('');
+
+            callback(null, reverseChankString + '\n');
+        },
+      });
+
+    await pipeline(
+        readableStream,
+        transformStream,
+        writableStream
+    );
 };
 
 await transform();
