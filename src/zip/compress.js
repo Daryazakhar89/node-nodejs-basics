e855246b255f11214183710447f79accb086a4cc
Darya Zakhar <daryazakhar.89@gmail.com>
2023-04-07 17:53:11 +0400

    feat: implement function that compresses file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import {createBrotliCompress } from 'zlib';
+
+const source = createReadStream('./src/zip/files/fileToCompress.txt');
+const destination = createWriteStream('./src/zip/files/archive.gz');
+const compression = createBrotliCompress();
+
 const compress = async () => {
-    // Write your code here 
+    await pipeline(source, compression, destination);
 };
 
 await compress();
